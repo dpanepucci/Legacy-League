ee19f74828975c44465d02d1d0d46bad3d87706f
Dylan Panepucci <dylan.panepucci@gmail.com>
2025-04-25 17:06:40 -0400

    OpenAI feature added and working, need to work on prompting and player selection and styling

---
+++ client/src/components/BattlePage/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+import SimulateMatchup from '../PlayersPage/SimulateMatchup';
+
+const MainLogin = () => {
+  
+  return (
+    <div>
+      <h3>MJ vs Lebron? Larry Bird vs Magic? Countless epic battles await! Who you got?</h3>
+
+      {Auth.loggedIn() ? (
+        <>
+        <SimulateMatchup />
+        </>
+      ) : (
+        <p>
+          Slow down buddy, please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup</Link> before playing.
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default MainLogin;
