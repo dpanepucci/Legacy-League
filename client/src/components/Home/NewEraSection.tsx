847c82018b46ce0bcc948da2439ded8708aaa8e4
Dylan Panepucci <dylan.panepucci@gmail.com>
2025-04-30 20:57:04 -0400

    mini players changed

--- client/src/components/Home/NewEraSection.tsx
+++ client/src/components/Home/NewEraSection.tsx
@@ -1,27 +1,29 @@
+
 import './NewEraSection.css';
 
-const players = [
-  { name: 'LeBron James', img: '/images/lebron.png' },
-  { name: 'Stephen Curry', img: '/images/stephen.png' },
-  { name: 'Kevin Durant', img: '/images/kevin.png' },
-  { name: 'Giannis Antetokounmpo', img: '/images/giannis.png' },
-  { name: 'Luka Dončić', img: '/images/luka.png' },
-];
-
-const NewEraSection = () => {
+const NewEraSelection = () => {
   return (
-    <div className="newera-wrapper">
-      <h2 className="newera-title">The New Era</h2>
-      <div className="newera-circle">
-        {players.map((player, i) => (
-          <div className={`circle-slot slot${i + 1}`} key={i}>
-            <img src={player.img} alt={player.name} />
-            <p>{player.name}</p>
-          </div>
-        ))}
-      </div>
+    <>
+    
+    <div className="legends-wrapper2">
+      <h1 className="legends-title2"></h1>
+      <img
+        src="/images/larry.png"
+        alt="Bouncing Legend"
+        className="tracking-image2"
+      />
     </div>
+
+    <div className="legends-wrapper4">
+      <h1 className="legends-title4"></h1>
+      <img
+      src="/images/giannis.png"
+      alt="Bouncing Legend"
+      className="tracking-image4"
+    />
+    </div>
+</>
   );
 };
 
-export default NewEraSection;
+export default NewEraSelection;
