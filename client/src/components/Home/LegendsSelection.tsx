f843dede0227487567a3834c3ced382ad2488dff
Dylan Panepucci <dylan.panepucci@gmail.com>
2025-04-30 21:32:41 -0400

    updates to KOBE and CURRY

--- client/src/components/Home/LegendsSelection.tsx
+++ client/src/components/Home/LegendsSelection.tsx
@@ -7,20 +7,22 @@ const LegendsSelection = () => {
     <div className="legends-wrapper">
       <h1 className="legends-title"></h1>
       <img
-        src="/images/stephen.png"
+        src="/images/nba_wrap_06.png"
         alt="Bouncing Legend"
         className="tracking-image"
       />
     </div>
-        <div className="legends-wrapper3">
-        <h1 className="legends-title3"></h1>
-        <img
-          src="/images/hakeem.png"
-          alt="Bouncing Legend"
-          className="tracking-image3"
-        />
-      </div>
-    </>
+
+<div className="legends-wrapperSC">
+<img
+  src="/images/curry.png"
+  alt="Bouncing Legend"
+  className="tracking-imageSC"
+/>
+</div>
+
+</>
+
   );
 };
 
--- client/src/components/Home/NewEraSection.tsx
+++ client/src/components/Home/NewEraSection.tsx
@@ -1,28 +1,16 @@
-
 import './NewEraSection.css';
 
 const NewEraSelection = () => {
   return (
     <>
-    
-    <div className="legends-wrapper2">
-      <h1 className="legends-title2"></h1>
-      <img
-        src="/images/larry.png"
-        alt="Bouncing Legend"
-        className="tracking-image2"
-      />
-    </div>
-
-    <div className="legends-wrapper4">
-      <h1 className="legends-title4"></h1>
-      <img
-      src="/images/giannis.png"
-      alt="Bouncing Legend"
-      className="tracking-image4"
-    />
-    </div>
-</>
+      <div className="legends-wrapper2">
+        <img
+          src="/images/kobeB.png"
+          alt="New Era Legend"
+          className="tracking-image2"
+        />
+      </div>
+    </>
   );
 };
 
